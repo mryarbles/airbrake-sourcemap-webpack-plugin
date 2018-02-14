f84b0a5c64ef285268d8434e51cb3e3be88b7ed2
mryarbles <mryarbles@gmail.com>
2018-02-14 12:08:56 -0800

    wip

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+echo "pre-commit started"
 
 update_version(){
     npm version patch
@@ -13,5 +13,9 @@ update_version
 # show off the updated version
 npm version | head -1
 
+
 # track the change
 git add package.json
+
+
+echo "pre-commit finished"
